77ec6588643adec3d7fa17c7a5cd803184a2665a
yve5 <yves.rouille@gmail.com>
2024-08-01 19:09:35 +0200

    airbnb

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import Game from './phaser/showcase';
 
 import './App.css';
 
+// let aze = 'aze';
+
 const App = () => (
   <div className="App">
     <Game />
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iste
+      similique laborum error veniam, hic est sint debitis adipisci quas fuga,
+      nobis vitae quaerat animi dignissimos consectetur eius quidem labore.
+    </p>
   </div>
 );
 
--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: { environment: 'jsdom', globals: true },
-  plugins: [eslint(), react()],
+  plugins: [
+    // eslint({
+    //   // overrideConfigFile: './.eslintrc.js',
+    //   // overrideConfigFile: './eslint.config.mjs',
+    // }),
+    eslint(),
+    react(),
+  ],
   build: { outDir: './build' },
   assetsInclude: ['**/*.md'],
 });
